447c1f142a48969821f0bf42ebcc890379d03941
Benny Teh <bennyteh98@gmail.com>
2021-12-10 23:27:44 +0800

    navbar-mobile

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import logo from '../../assets/shared/logo.svg';
 
-const navbar = () => {
+const Navbar = () => {
+
+    const [openBurger, setOpenBurger] = useState(false);
+    const togglehamburger = () => {
+        setOpenBurger(!openBurger)
+    }
+
     return (
         <div className="space__navbar">
                 <div className="space__navbar_logo">
                     <img src={logo} alt="logo" />
                 </div>
-                <span className="space__navbar_line"></span>
+                <span className="space__navbar_line"></span>       
                 <div className="space__navbar_container">
-                    <p><a href="#home"><span>00</span> HOME</a></p>
-                    <p><a href="#destination"><span>01</span> DESTINATION</a></p>
-                    <p><a href="#crew"><span>02</span> CREW</a></p>
-                    <p><a href="#technology"><span>03</span> TECHNOLOGY</a></p>
-                </div> 
+                    <p><a href="#home"><span>00</span>HOME</a></p>
+                    <p><a href="#destination"><span>01</span>DESTINATION</a></p>
+                    <p><a href="#crew"><span>02</span>CREW</a></p>
+                    <p><a href="#technology"><span>03</span>TECHNOLOGY</a></p>
+                </div>
+                <button onClick={togglehamburger}></button>
+                {openBurger && 
+                <div className="space__navbar_container-mobile">
+                    <p><a href="#home"><span>00</span>HOME</a></p>
+                    <p><a href="#destination"><span>01</span>DESTINATION</a></p>
+                    <p><a href="#crew"><span>02</span>CREW</a></p>
+                    <p><a href="#technology"><span>03</span>TECHNOLOGY</a></p>
+                </div>}
         </div>
     )
 }
 
-export default navbar
+export default Navbar

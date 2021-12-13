bbd5d7197243d89ee810946538ec3beeeb33c1cc
Benny Teh <bennyteh98@gmail.com>
2021-12-13 21:45:58 +0800

    responsive home

--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -4,16 +4,18 @@ import {Navbar} from '../../components/components'
 
 const Home = () => {
     return (
-        <div className='space__home'>
-            <Navbar />
+        <div className='space__home' id="home">
+            <Navbar className="space__home__navbar"/>
             <div className='space__home_container'>
                 <header className='space__home_container_description'>
-                    <h3>So, you want to travel to</h3>
+                    <h3>SO, YOU WANT TO TRAVEL TO</h3>
                     <h1>SPACE</h1>
                     <p>
-                    Let's face it; if you want to go to space, you might as well genuinely go to 
-                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                    because we'll give you a truly out of this world experience!
+                    Let's face it; if you want to go to space, 
+                    you might as well genuinely go to outer space 
+                    and not hover kind of on the edge of it. Well 
+                    sit back, and relax because we'll give you a truly 
+                    out of this world experience!
                     </p>
                 </header>
 
